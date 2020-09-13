27c8c197c4c52eac27d8a3fe1178ab41f6369d9f
windy <71083075+windyoc28@users.noreply.github.com>
2020-09-14 01:05:39 +0700

    membuat class mobil

--- index.php
+++ index.php
@@ -1,3 +1,22 @@
 <?php
 
-	echo "Hello World";
+	class Mobil {
+		public $merk, $tipe, $mesin, $max_speed;
+
+		public function cetakTipe(){
+			return $this->tipe;
+		}
+
+		function kecepatanMaksimal(){
+			return "kecepatan maksimal dari mobil ini adalah ".$this->max_speed;
+		}
+	}
+
+	$bmw = new mobil;
+	$bmw->merk = "bmw";
+	$bmw->tipe = "3201";
+	$bmw->mesin = "200cc";
+	$bmw->max_speed = "200km/h";
+
+echo "Tipe".$bmw->kecepatanMaksimal();
+
